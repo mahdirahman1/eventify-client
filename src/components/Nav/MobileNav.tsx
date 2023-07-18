78e1b603891e63fa96bb1edf11f5afc0ec56814e
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2023-07-17 23:46:08 -0400

    update mobile nav logo title

--- src/components/Nav/MobileNav.tsx
+++ src/components/Nav/MobileNav.tsx
@@ -31,7 +31,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
 			/>
 
 			<Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-				Logo
+				Eventify
 			</Text>
 		</Flex>
 	);
